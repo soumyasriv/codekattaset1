ab4898a9c0c297261d8190d58fe322393643bad1
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-20 09:49:18 -0800

    Update leap.java

--- leap.java
+++ leap.java
@@ -5,13 +5,28 @@ public static void main(String[] args)
 {
 Scanner sc= new Scanner(System.in)
 int n=sc.nextInt();
-if (n%4==0 && n%100!=0 && n%400==0)
+if (n%4==0)
 {
-System.out.println("Leap Year");
+  if (n%100==0)
+  {
+    if (n%400==0)
+    {
+      System.out.println("Leap Year");
+      }
+    else 
+     {
+      System.out.println("Not a leap year");
+    }
+  }
+  else
+    {
+    System.out.println("Leap Year");
+    }
 }
-else 
+else
 {
-System.out.println("Not a leap year");
+   System.out.println("Not a leap year");
+  }
 }
 }
 
