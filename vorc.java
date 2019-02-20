490f282c345729271861eeb6efc8fba396710521
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-20 06:26:58 -0800

    Create vorc.java

---
+++ vorc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution
+{
+public static void main (String[] args)
+{
+Scanner sc=new Scanner(System.in);
+char c=sc.next().charAt(0);
+if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+System.out.println("Vowel");
+else
+System.out.println("Consonant");
+}}
