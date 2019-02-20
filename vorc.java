ef590ec2b17498d539eb4bcd03b8be02a058d475
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-20 06:37:21 -0800

    Update vorc.java

--- vorc.java
+++ vorc.java
@@ -7,6 +7,8 @@ Scanner sc=new Scanner(System.in);
 char c=sc.next().charAt(0);
 if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
 System.out.println("Vowel");
+else if (c=='$')
+System.out.println("Invalid");
 else
 System.out.println("Consonant");
 }}
